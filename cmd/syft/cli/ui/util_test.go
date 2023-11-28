13d9423be0d6f3ad142663897a10d1820bba86c4
Alex Goodman <wagoodman@users.noreply.github.com>
2023-11-28 11:16:46 -0500

    simplify batch message processing for bubbletea
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- cmd/syft/cli/ui/util_test.go
+++ cmd/syft/cli/ui/util_test.go
@@ -4,7 +4,6 @@ import (
 	"reflect"
 	"sync"
 	"testing"
-	"unsafe"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -40,33 +39,18 @@ func runModel(t testing.TB, m tea.Model, iterations int, message tea.Msg, h ...*
 	return m.View()
 }
 
-func flatten(p tea.Msg) (msgs []tea.Msg) {
-	ty := reflect.TypeOf(p)
-	name := ty.Name()
-	if name == "BatchMsg" {
-		partials := extractBatchMessages(p)
-		for _, m := range partials {
-			msgs = append(msgs, flatten(m)...)
+func flatten(ps ...tea.Msg) (msgs []tea.Msg) {
+	for _, p := range ps {
+		if bm, ok := p.(tea.BatchMsg); ok {
+			for _, m := range bm {
+				if m == nil {
+					continue
+				}
+				msgs = append(msgs, flatten(m())...)
+			}
+		} else {
+			msgs = []tea.Msg{p}
 		}
-	} else {
-		msgs = []tea.Msg{p}
 	}
 	return msgs
-}
-
-func extractBatchMessages(m tea.Msg) (ret []tea.Msg) {
-	sliceMsgType := reflect.SliceOf(reflect.TypeOf(tea.Cmd(nil)))
-	value := reflect.ValueOf(m) // note: this is technically unaddressable
-
-	// make our own instance that is addressable
-	valueCopy := reflect.New(value.Type()).Elem()
-	valueCopy.Set(value)
-
-	cmds := reflect.NewAt(sliceMsgType, unsafe.Pointer(valueCopy.UnsafeAddr())).Elem()
-	for i := 0; i < cmds.Len(); i++ {
-		item := cmds.Index(i)
-		r := item.Call(nil)
-		ret = append(ret, r[0].Interface().(tea.Msg))
-	}
-	return ret
 }
